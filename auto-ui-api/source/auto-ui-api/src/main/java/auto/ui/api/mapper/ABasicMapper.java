c2910a5e35d369d9484a3c030d3135c361817412
huypham <huyphuocbinh204@gmail.com>
2026-08-27 09:36:25 +0700

    fix

--- auto-ui-api/source/auto-ui-api/src/main/java/auto/ui/api/mapper/ABasicMapper.java
+++ auto-ui-api/source/auto-ui-api/src/main/java/auto/ui/api/mapper/ABasicMapper.java
@@ -1,8 +1,10 @@
 package auto.ui.api.mapper;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 
+@Mapper(componentModel = "spring")
 public interface ABasicMapper {
 
     @Named("jsonNodeToString")
